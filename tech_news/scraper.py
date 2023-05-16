b67b409198700167af0b3b3ceb10fb2e5c8eb751
Victor Mendes <victorutdsim7@gmail.com>
2023-05-16 20:32:40 -0300

    [req-5]-feat: função get_tech_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import requests
 from requests.exceptions import ReadTimeout
 from time import sleep
 from parsel import Selector
+from tech_news.database import create_news
 import re
 
 
@@ -30,6 +31,7 @@ def scrape_next_page_link(html_content):
 
 def scrape_news(html_content):
     selector = Selector(text=html_content)
+
     url = selector.css("link[rel='canonical']::attr(href)").get()
     title = selector.css("h1.entry-title::text").get()
     timestamp = selector.css("li.meta-date::text").get()
@@ -54,6 +56,23 @@ def scrape_news(html_content):
     }
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    endpoint = "https://blog.betrybe.com/"
+    home_html = fetch(endpoint)
+    main_page_content = scrape_updates(home_html)
+    links_to_scrape = []
+    links_to_scrape.extend(main_page_content[:amount])
+
+    next_page_link = scrape_next_page_link(home_html)
+    while len(links_to_scrape) < amount:
+        next_page_content = fetch(next_page_link)
+        links_to_scrape.extend(scrape_updates(next_page_content))
+        next_page_link = scrape_next_page_link(next_page_content)
+
+    scraped_content = []
+    for link in links_to_scrape[:amount]:
+        html = fetch(link)
+        scraped_content.append(scrape_news(html))
+
+    create_news(scraped_content)
+    return scraped_content
