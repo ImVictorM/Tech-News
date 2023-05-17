996e7b2cee86248cf1b06398a93d80061b5823fe
Victor Mendes <victorutdsim7@gmail.com>
2023-05-17 15:47:17 -0300

    [req-10]-feat: função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,22 @@
-# Requisito 10
+from tech_news.database import db
+from collections import Counter
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_from_db = list(db.news.find({}, {"_id": False, "category": True}))
+
+    categories_list = [element["category"] for element in news_from_db]
+
+    categories_count = Counter(categories_list)
+
+    top_5_categories = [
+        category
+        for category, _category_count in categories_count.most_common(5)
+    ]
+
+    top_5_sorted = sorted(
+        top_5_categories,
+        key=lambda category: (-categories_count[category], category),
+    )
+
+    return top_5_sorted
